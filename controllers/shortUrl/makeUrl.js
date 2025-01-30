1ec1caff8b84015f914bca48070ed3044b912a7b
Md Mubin <fontdevmubin@gmail.com>
2025-01-30 22:11:44 +0600

    modified:   controllers/shortUrl/makeUrl.js

--- controllers/shortUrl/makeUrl.js
+++ controllers/shortUrl/makeUrl.js
@@ -26,7 +26,7 @@ const makeUrl = async (req, res) => {
         return res.render("homePage" ,{
             msg: "Short Url Created Successfully",
             bigUrl: existBigUrl.bigUrl,
-            shortUrl: `${process.env.BASE_URL}/${existBigUrl.shortUrl}`
+            shortUrl: `http://mubin/${existBigUrl.shortUrl}`
         })
     }
 
@@ -40,7 +40,7 @@ const makeUrl = async (req, res) => {
     res.render("homepage" , {
         msg: "Short Url Created Successfully",
         bigUrl: newShortUrl.bigUrl,
-        shortUrl: `${process.env.BASE_URL}/${newShortUrl.shortUrl}`
+        shortUrl: `http://mubin//${newShortUrl.shortUrl}`
     })
 }
 
