a6b614974ef473151157135638a1a24e4995399a
Md Mubin <fontdevmubin@gmail.com>
2025-01-31 23:48:31 +0600

    Changes to be committed: 	modified:   controllers/auth/login.js 	modified:   views/loginPage.ejs

--- controllers/auth/login.js
+++ controllers/auth/login.js
@@ -3,33 +3,37 @@ const registerSchema = require("../../modal/registerSchema")
 const login = async (req, res) => {
     const {email , pass} = req.body
 
-    if(!email){
-        return res.render("loginPage", {
-            error : "Email required"
-        })
+    try {
+        if(!email){
+            return res.render("loginPage", {
+                error : "Email required"
+            })
+        }
+    
+        if(!pass){
+            return res.render("loginPage", {
+                error : "Password required"
+            })
+        }
+    
+        const registeredUser = await registerSchema.findOne({email})
+    
+        if(!registeredUser){
+            return res.render("loginPage", {
+                error : "No User Found"
+            })
+        }
+    
+        if(registeredUser.pass !== pass){
+            return res.render("loginPage", {
+                error : "No User Found"
+            })
+        }
+    
+        res.send(registeredUser)
+    } catch (error) {
+        res.status(400).send("Server Error!")
     }
-
-    if(!pass){
-        return res.render("loginPage", {
-            error : "Password required"
-        })
-    }
-
-    const registeredUser = await registerSchema.findOne({email})
-
-    if(!registeredUser){
-        return res.render("loginPage", {
-            error : "No User Found"
-        })
-    }
-
-    if(registeredUser.pass !== pass){
-        return res.render("loginPage", {
-            error : "No User Found"
-        })
-    }
-
-    res.send(registeredUser)
 }
 
 module.exports = login
