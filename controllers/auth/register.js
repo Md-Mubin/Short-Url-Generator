3a5214e548852ca5cb60772f75ee0dc51fe8b3cc
Md Mubin <fontdevmubin@gmail.com>
2025-01-31 23:34:43 +0600

    modified:   controllers/auth/register.js

--- controllers/auth/register.js
+++ controllers/auth/register.js
@@ -6,30 +6,36 @@ const register = async (req, res) => {
     const { userName, email, pass } = req.body
 
     try {
+
+        // if user name is empty
         if (!userName) {
             return res.render("registerPage",{
                 error: "Name Required"
             })
         }
 
+        // if email is empty
         if (!email) {
             return res.render("registerPage",{
                 error: "Email Required"
             })
         }
 
+        // if email is not  validated
         if (!emailValidator(email)) {
             return res.render("registerPage",{
                 error: "Email is not Validated"
             })
         }
 
+        // if password is empty
         if (!pass) {
             return res.render("registerPage",{
                 error: "Password Required"
             })
         }
 
+        // variable for dynamic passowrd validate error
         const passwordValidateResult = passwordValidator(pass)
 
         if (passwordValidateResult) {
@@ -38,15 +44,27 @@ const register = async (req, res) => {
             })
         }
 
-        const userCreate = await registerSchema({
+        // if user with email already exists
+        const existUser = await registerSchema.find({email})
+
+        if(existUser){
+            return res.render("registerPage",{
+                error: "Email alredy in use"
+            })
+        }
+        
+        // create user
+        const userCreate = registerSchema({
             userName, email, pass
         })
 
         userCreate.save()
 
+        // register successfull massage
         res.render("registerPage",{
             msg: "Register Successfull!"
         })
+
     } catch (error) {
         res.render({
             error: "Server Error"
