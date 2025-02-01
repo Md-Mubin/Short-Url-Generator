0f8f518419708444ea584a9079a18db6e660a98f
Md Mubin <fontdevmubin@gmail.com>
2025-02-01 21:35:02 +0600

    modified:   index.js

--- index.js
+++ index.js
@@ -9,10 +9,18 @@ const app = express({caseSensitive : true})
 
 // =============== All Uses
 app.use(express.json())
+
+// ejs engine 
 app.set("view engine", "ejs")
 app.use(express.static(__dirname + "/public"))
+
+// for sending url 
 app.use(express.urlencoded({extended : false}))
+
+// app router
 app.use(router)
+
+// data base connect
 dbConnect()
 
 // =============== local
