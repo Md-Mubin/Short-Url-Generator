16d825bfe46e9cceb792c1e6c6ea95abd9a17116
Md Mubin <fontdevmubin@gmail.com>
2025-02-16 22:59:12 +0600

    Changes to be committed: 	modified:   middlewares/authMiddleware.js

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -4,14 +4,18 @@ const authMiddleware = (req, res, next) => {
     try {
         const tokken = req.cookies
 
-        if (!tokken.access_token) return res.status(400).send("Un-Authorized")
-
-        const decoded = jwt.verify(tokken.access_token, process.env.ACSTOKEN)
-
-        if (decoded.data) {
-            req.user = decoded.data
+        if (tokken.access_token){   
+            const decoded = jwt.verify(tokken.access_token, process.env.ACSTOKEN)
+            
+            if (decoded.data) {
+                req.user = decoded.data
+                next()
+            }
+        }else{
+            req.user = null
             next()
         }
+        
     } catch (error) {
         res.status(500).send("Server Side Error")
     }
