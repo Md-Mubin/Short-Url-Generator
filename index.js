b00069cc6600bc3eaecc15d93077316e1202bc47
Md Mubin <fontdevmubin@gmail.com>
2025-02-16 21:25:06 +0600

    modified:   index.js

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const cookieParser = require("cookie-parser")
 
 // =============== All Uses
 app.use(express.json())
-app.use(cookieParser)
+app.use(cookieParser())
 
 // ejs engine 
 app.set("view engine", "ejs")
