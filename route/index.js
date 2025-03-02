d2eb027379006984c434af0e6bd9c3ffe85c5057
Md Mubin <fontdevmubin@gmail.com>
2025-03-02 23:19:07 +0600

    Changes to be committed: 	modified:   route/index.js

--- route/index.js
+++ route/index.js
@@ -8,14 +8,17 @@ const router = express.Router()
 
 router.use(process.env.BASE_URL_API, apiRoute)
 
+// home, register and login static page
 router.get("/", authMiddleware, home)
 router.get("/register", register)
 router.get("/login", login)
 
+// for logout
 router.post("/logout", (req,res)=>{
     res.clearCookie("access_token").redirect("/login")
 })
 
+// for dashboard
 router.get("/dashboard", authMiddleware, async (req,res)=>{
     if(req.user){
         const loggedUserInfo = await registerSchema.findById(req.user.id).select("-pass").populate("shortUrls")
@@ -28,8 +31,10 @@ router.get("/dashboard", authMiddleware, async (req,res)=>{
     }
 })
 
+// short url render route
 router.get("/:shortUrl", renderUrl)
 
+// global any route
 router.use((req, res) => {
     res.render("noPage")
 })
